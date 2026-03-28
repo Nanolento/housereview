a4aeb4c495349ec155168c931f479dd17de91c62
Jelmer <jelmersmit@upcmail.nl>
2026-03-28 15:16:28 +0100

    Added loading house data into the database
    - currently on a /load test route, will rework later

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\HouseLoader;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +20,15 @@ class DashboardController extends AbstractController {
     #[Route('/')]
     public function main(): Response {
         return new Response('test');
+    }
+
+    // This is a test route to make loading houses possible while the ui isnt finished,
+    // we will make it so it loads the houses automatically if none were found with the ui.
+    #[Route('/load')]
+    public function loadHouse(HouseLoader $hl): Response {
+        $houses = $hl->loadHouses();
+        dump($houses);
+        return new Response('<html><body><h1>hello</h1></body></html>');
     }
     
 }
---
+++ src/Service/HouseLoader.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Service;
+
+use App\Entity\House;
+use Doctrine\ORM\EntityManagerInterface;
+
+/*
+The HouseLoader class takes houses from the JSON data and puts them in the SQL
+database. If run when the database is not empty, it will add the houses that were
+not yet in the database to the database.
+*/
+
+class HouseLoader {
+
+    public function __construct(private string $filePath, private EntityManagerInterface $em) {}
+    
+    public function loadHouses() {
+        # Load JSON data
+        $housefile = file_get_contents($this->filePath);
+        $houses = json_decode($housefile, true); # associative because arrays are easier
+
+        # Process each house
+        for ($i = 0; $i < count($houses); $i++) {
+            # Create house object and set values
+            $house = new House();
+            $house->setExternalId($houses[$i]['listing_id']);
+            $house->setTitle($houses[$i]['headline']);
+            $house->setMonthlyRent($houses[$i]['monthly_rent']);
+            $house->setEnergyLabel($houses[$i]['energy_class']);
+            $house->setCity($houses[$i]['location_city']);
+
+            # save this house
+            $this->em->persist($house);
+        }
+        # actually write them to the database.
+        $this->em->flush();
+        return $houses;
+    }
+}
