1c48e0eac1d935d5c1ede6b4eadd5293fe333ca2
Jelmer <jelmersmit@upcmail.nl>
2026-03-29 17:50:44 +0200

    Refactored dashboard template to use House object
    - passing all properties individually was getting a bit long and passing
      the House object itself is needed for Live Component functionality
      later.

--- src/Twig/Components/HouseListing.php
+++ src/Twig/Components/HouseListing.php
@@ -1,8 +1,7 @@
 <?php
 namespace App\Twig\Components;
 
-use App\Enum\Grade;
-use App\Enum\HouseStatus;
+use App\Entity\House;
 
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
@@ -10,22 +9,8 @@ use Symfony\UX\LiveComponent\DefaultActionTrait;
 #[AsLiveComponent]
 class HouseListing {
     use DefaultActionTrait;
+
+    #[LiveProp(writable: ['status'])]
+    public House $house;
     
-    public string $title = 'House';
-    public int $monthlyRent = 0;
-    public string $city = 'Amsterdam';
-    public string $energyLabel = 'A';
-
-    public Grade $rentGrade = Grade::REJECTED;
-    public Grade $titleGrade = Grade::WARNING;
-    public Grade $energyGrade = Grade::GOOD;
-
-    public Grade $overallGrade = Grade::REJECTED;
-
-    public HouseStatus $status = HouseStatus::PENDING;
-
-    # Note: the below values are defined as string to make
-    # it possible to write "(not specified)" if the values are null
-    public string $roomCountString = "2";
-    public string $areaString = "50 m2";
 }
