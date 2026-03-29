82cab551b0d79b98db7253d7569336d7c5648d6c
Jelmer <jelmersmit@upcmail.nl>
2026-03-29 19:41:20 +0200

    Approving the house now possible live via button in ui
    - uses live components to automatically change the value in the ui and
      update the db in the background.
    - we will use buttons to allow adjustment of the status as a select
      statement was proving quite difficult and overcomplicated.

--- src/Twig/Components/HouseListing.php
+++ src/Twig/Components/HouseListing.php
@@ -2,15 +2,31 @@
 namespace App\Twig\Components;
 
 use App\Entity\House;
+use App\Enum\HouseStatus;
 
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+
+use Doctrine\ORM\EntityManagerInterface;
 
 #[AsLiveComponent]
 class HouseListing {
+
+    public function __construct(
+        private EntityManagerInterface $em
+    ) {}
+    
     use DefaultActionTrait;
 
-    #[LiveProp(writable: ['status'])]
+    #[LiveProp]
     public House $house;
-    
+
+    #[LiveAction]
+    public function approveHouse()
+    {
+        $this->house->setStatus(HouseStatus::APPROVED);
+        $this->em->flush();
+    }
 }
